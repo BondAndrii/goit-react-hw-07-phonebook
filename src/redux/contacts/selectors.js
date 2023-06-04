fda7a454b91a59bb1c0ede4ffe8ab8d464d709f7
BondAndrii <104005997+BondAndrii@users.noreply.github.com>
2023-06-04 21:51:43 +0300

    update selectFilteredContacts

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,3 +1,7 @@
+// import { useSelector } from "react-redux";
+
+import { selectFilter } from "redux/filter/selectors";
+
 export const selectContacts = state => state.contacts.items;
 
 export const selectIsLoading = state => state.contacts.isLoading;
@@ -5,15 +9,18 @@ export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 
 export const selectFilteredContacts = state => {
-    const { contacts, filter } = state;
-    const { items } = contacts;
-     if (!filter) {
-      return items;
+    const itm = selectContacts(state);
+    const filtr = selectFilter(state);
+
+    // const { contacts, filter } = state;
+    // const { items } = contacts;
+     if (!filtr) {
+      return itm;
      } else {
-        const normalizedFilter = filter.toLowerCase();
-        const findAbonent = items.filter(({ name, number }) => name.toLowerCase().trim().includes(normalizedFilter) || 
+        const normalizedFilter = filtr.toLowerCase();
+        const findAbonent = itm.filter(({ name, number }) => name.toLowerCase().trim().includes(normalizedFilter) || 
             number.trim().includes(normalizedFilter)); 
-         console.log("findAbonent", findAbonent)
+        //  console.log("findAbonent", findAbonent)
          return findAbonent;
     }
 }
